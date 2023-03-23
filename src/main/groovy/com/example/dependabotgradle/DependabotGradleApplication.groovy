9aa2d81d8ccb855a5335c0ce0e374fc99823ccb2
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2023-03-23 13:59:18 +1100

    depdendency

---
+++ src/main/groovy/com/example/dependabotgradle/DependabotGradleApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.dependabotgradle
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DependabotGradleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DependabotGradleApplication, args)
+	}
+
+}
